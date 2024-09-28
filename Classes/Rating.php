689fee8a2d7cbe346c858db4dac160997ddd897e
Anusprasan <anushprasan20001112@gmail.com>
2024-09-28 16:08:23 +0530

    rating file created

---
+++ Classes/Rating.php
@@ -0,0 +1,18 @@
+<?php
+class Rating{
+    public $con;
+    private $table = "rating";
+    public $ratingId;
+    public $driverId;
+    public $passengerId;
+    public $tripId;
+    public $score;
+    public $datetime;
+    public $review;
+    public $active;
+    
+
+
+
+} 
+?>
