bcb4d4a768bb7c2be7605a341bbf26c156614d48
Anusprasan <anushprasan20001112@gmail.com>
2024-09-28 16:26:10 +0530

    Payment class created

---
+++ Classes/Payment.php
@@ -0,0 +1,19 @@
+<?php
+class Payment {
+    public $con;
+    private $table = "payments"
+
+    public $paymentId;
+    public $tripId;
+    public $paidBy;
+    public $driverId;
+    public $paymentmethod;
+    public $paymentStatus;
+    public $paidDateTime;
+    public $active;
+
+
+
+}
+
+?>
