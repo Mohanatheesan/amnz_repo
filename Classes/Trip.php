5345134567eda8d8b8c6b23623f1924865e1ee8b
Anusprasan <anushprasan20001112@gmail.com>
2024-09-28 16:39:15 +0530

    trip class created

---
+++ Classes/Trip.php
@@ -0,0 +1,23 @@
+<?php
+class Trip{
+    public $con;
+    private $table = "trips";
+    
+    public $tripId;
+    public $reservationId;
+    public $driverId;
+    public $status;
+    public $arrivedTime;
+    public $pickedupTime;
+    public $droppedoffTime;
+    public $amount;
+    public $note;
+    public $active;
+
+
+
+
+}
+
+
+?>
